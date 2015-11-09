5a5f98c6ead7061db9d323bf4dd402c21b31c1f6
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-11-09 01:55:19 -0500

    Added basic support for receiving all unconnected messages

---
+++ src/Interchange.Playground/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Interchange.Playground
+{
+    public class Program
+    {
+        public void Main(string[] args) {
+            Node node = new Node();
+            node.ProcessIncomingMessageAction = HandleIncomingPacket;
+            node.ListenAsync(IPAddress.Any, 5000).Wait();
+
+            while (true) {
+                Task.Delay(1);
+            }
+        }
+
+        private void HandleIncomingPacket(ArraySegment<byte> buffer) {
+            Console.WriteLine(buffer.Count);
+        }
+    }
+}
---
+++ src/Interchange/Node.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Interchange
+{
+    public class Node
+    {
+        public static readonly int BufferSize = 1014;
+
+        Socket socket;
+
+        byte[] buffer;
+
+        SocketAsyncEventArgs readEventArgs;
+
+        public Action<ArraySegment<byte>> ProcessIncomingMessageAction { get; set; }
+
+        public Node() {
+            buffer = new byte[BufferSize];
+
+            readEventArgs = new SocketAsyncEventArgs();
+            readEventArgs.SetBuffer(buffer, 0, buffer.Length);
+            readEventArgs.Completed += ReadEventArgs_Completed;
+            readEventArgs.RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        }
+
+        public async Task ListenAsync(IPAddress localIPAddress, int port) {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint ipEndPoint = new IPEndPoint(localIPAddress, port);
+            socket.Bind(ipEndPoint);
+
+            socket.ReceiveFromAsync(readEventArgs);
+        }
+
+        private void PerformReceive(SocketAsyncEventArgs e) {
+            bool willRaiseEvent = socket.ReceiveFromAsync(e);
+            if (!willRaiseEvent) {
+                HandlePacketReceived(e);
+            }
+        }
+
+        private void ReadEventArgs_Completed(object sender, SocketAsyncEventArgs e) {
+            switch (e.LastOperation) {
+                case SocketAsyncOperation.ReceiveFrom:
+                    HandlePacketReceived(e);
+                    PerformReceive(e);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private void HandlePacketReceived(SocketAsyncEventArgs e) {
+            ArraySegment<byte> segment = new ArraySegment<byte>(e.Buffer, e.Offset, e.BytesTransferred);
+
+            ProcessIncomingMessageAction(segment);
+        }
+    }
+}
