87275131946b83aeda1cf9e22d0a85499ffcd495
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-11-08 23:26:20 -0500

    Preliminary support for multiplatform build

---
+++ src/Interchange/Platforms/DotNet/NodeBase.cs
@@ -0,0 +1,29 @@
+﻿#if DOTNET
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Interchange
+{
+    public class NodeBase
+    {
+        Socket socket;
+
+        public NodeBase() {
+
+        }
+
+        public void Listen(IPAddress localIPAddress, int port) {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint ipEndPoint = new IPEndPoint(localIPAddress, port);
+            socket.Bind(ipEndPoint);
+        }
+    }
+}
+
+#endif
---
+++ src/Interchange/Platforms/WinRT/NodeBase.cs
@@ -0,0 +1,15 @@
+﻿#if WINRT
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Interchange
+{
+    public class NodeBase
+    {
+    }
+}
+
+#endif
